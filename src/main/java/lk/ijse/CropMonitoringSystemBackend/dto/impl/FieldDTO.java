c26d432fd6fc920bbabe078a3db42ec14e0701d6
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-10-29 14:20:02 +0530

    create FieldDTO

---
+++ src/main/java/lk/ijse/CropMonitoringSystemBackend/dto/impl/FieldDTO.java
@@ -0,0 +1,23 @@
+package lk.ijse.CropMonitoringSystemBackend.dto.impl;
+
+import lk.ijse.CropMonitoringSystemBackend.dto.SuperDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.awt.*;
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FieldDTO implements SuperDTO {
+    private String field_code;
+    private String field_name;
+    private Point location;
+    private Double extent_size;
+    private String image1;
+    private String image2;
+    private List<CropDTO> cropDTOS;
+    private List<StaffDTO> staffDTOS;
+
+}
