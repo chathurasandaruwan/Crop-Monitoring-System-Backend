03a64502504158054fc0cb26bd2d5db3f6fe637d
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-10-29 12:02:47 +0530

    first commit

---
+++ src/test/java/lk/ijse/CropMonitoringSystemBackend/CropMonitoringSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.CropMonitoringSystemBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropMonitoringSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
