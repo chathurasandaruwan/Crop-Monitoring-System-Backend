0ed32edeaefbdf952ea9764997624abb0b62d27e
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-10-30 09:30:07 +0530

    create UserDTO

---
+++ src/main/java/lk/ijse/CropMonitoringSystemBackend/dto/impl/UserDTO.java
@@ -0,0 +1,17 @@
+package lk.ijse.CropMonitoringSystemBackend.dto.impl;
+
+import lk.ijse.CropMonitoringSystemBackend.dto.SuperDTO;
+import lk.ijse.CropMonitoringSystemBackend.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO implements SuperDTO {
+    private String user_id;
+    private String email;
+    private String password;
+    private Role role;
+}
