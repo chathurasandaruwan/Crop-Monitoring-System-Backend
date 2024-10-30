2d6e8a13288deb1b26dde4e4d0cb8a20cbc70ad2
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-10-30 09:27:32 +0530

    create EquipmentDTO

---
+++ src/main/java/lk/ijse/CropMonitoringSystemBackend/dto/impl/EquipmentDTO.java
@@ -0,0 +1,18 @@
+package lk.ijse.CropMonitoringSystemBackend.dto.impl;
+
+import lk.ijse.CropMonitoringSystemBackend.dto.SuperDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EquipmentDTO implements SuperDTO {
+
+    private String equipment_id;
+    private String name;
+    private String status;
+    private FieldDTO field;
+    private StaffDTO staff;
+}
