f90bae6bef4a6b25d537fe253fba05732d5e821a
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-10-31 12:07:57 +0530

    change FIELD regex patton

--- src/main/java/lk/ijse/CropMonitoringSystemBackend/util/RegexProcess.java
+++ src/main/java/lk/ijse/CropMonitoringSystemBackend/util/RegexProcess.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class RegexProcess {
     public static boolean userIdMatcher(String userId) {
-        String regexForUserID = "^USER-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
+        String regexForUserID = "^FIELD-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
         Pattern regexPattern = Pattern.compile(regexForUserID);
         return regexPattern.matcher(userId).matches();
     }
