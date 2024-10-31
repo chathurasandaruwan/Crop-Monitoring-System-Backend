d3fada65536c14a7d5b59b52f654fd2dd29903fa
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-10-31 12:12:33 +0530

    create FieldService layer

---
+++ src/main/java/lk/ijse/CropMonitoringSystemBackend/service/FieldService.java
@@ -0,0 +1,4 @@
+package lk.ijse.CropMonitoringSystemBackend.service;
+
+public interface FieldService {
+}
---
+++ src/main/java/lk/ijse/CropMonitoringSystemBackend/service/impl/FieldServiceImpl.java
@@ -0,0 +1,11 @@
+package lk.ijse.CropMonitoringSystemBackend.service.impl;
+
+import jakarta.transaction.Transactional;
+import lk.ijse.CropMonitoringSystemBackend.service.FieldService;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class FieldServiceImpl implements FieldService {
+
+}
