02aac2b104ffd5bd8ac7423eb7b5a97ef8133822
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-11-01 12:53:46 +0530

    create saveCrop endpoint

---
+++ src/main/java/lk/ijse/CropMonitoringSystemBackend/controller/CropController.java
@@ -0,0 +1,47 @@
+package lk.ijse.CropMonitoringSystemBackend.controller;
+
+import lk.ijse.CropMonitoringSystemBackend.dto.impl.CropDTO;
+import lk.ijse.CropMonitoringSystemBackend.exeption.DataPersistException;
+import lk.ijse.CropMonitoringSystemBackend.util.AppUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("api/v1/crop")
+public class CropController {
+    public ResponseEntity<Void> saveCrop(
+            @RequestPart("common_name") String common_name,
+            @RequestPart("scientific_name") String scientific_name,
+            @RequestPart("image")MultipartFile image,
+            @RequestPart("category")String category,
+            @RequestPart("season") String season,
+            @RequestPart("field_code")String field_code
+            ){
+        //generate crop code
+        String cropCode = AppUtil.generateCropCode();
+        String imageBase64 = "";
+        try {
+            imageBase64 = AppUtil.imageToBase64(image.getBytes());
+            CropDTO cropDTO = new CropDTO();
+            cropDTO.setCrop_code(cropCode);
+            cropDTO.setCommon_name(common_name);
+            cropDTO.setScientific_name(scientific_name);
+            cropDTO.setImage(imageBase64);
+            cropDTO.setCategory(category);
+            cropDTO.setSeason(season);
+            cropDTO.setField_code(field_code);
+            return null;
+            //todo; call service layer
+        }catch (DataPersistException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
--- src/main/java/lk/ijse/CropMonitoringSystemBackend/util/AppUtil.java
+++ src/main/java/lk/ijse/CropMonitoringSystemBackend/util/AppUtil.java
@@ -7,6 +7,9 @@ public class AppUtil {
     public static String generateFieldCode() {
         return "FIELD-"+ UUID.randomUUID();
     }
+    public static String generateCropCode() {
+        return "CROP-"+ UUID.randomUUID();
+    }
     public static String imageToBase64(byte[] image){
         return Base64.getEncoder().encodeToString(image);
     }
