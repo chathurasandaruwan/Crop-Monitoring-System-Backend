01fdcc9f81068fc7fce8cf4bbfb3546373ff71c9
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-11-09 04:51:52 +0530

    create StaffDAO

---
+++ src/main/java/lk/ijse/CropMonitoringSystemBackend/dao/StaffDAO.java
@@ -0,0 +1,14 @@
+package lk.ijse.CropMonitoringSystemBackend.dao;
+
+import lk.ijse.CropMonitoringSystemBackend.entity.impl.StaffEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface StaffDAO extends JpaRepository<StaffEntity, String> {
+
+}
+
