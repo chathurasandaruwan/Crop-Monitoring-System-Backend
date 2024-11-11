2dd43d5f3232ae9573bc72f2541654218a624615
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-11-11 06:10:28 +0530

    create  vehicle delete service layer

--- src/main/java/lk/ijse/CropMonitoringSystemBackend/service/impl/VehicleServiceImpl.java
+++ src/main/java/lk/ijse/CropMonitoringSystemBackend/service/impl/VehicleServiceImpl.java
@@ -5,12 +5,14 @@ import lk.ijse.CropMonitoringSystemBackend.dao.VehicleDAO;
 import lk.ijse.CropMonitoringSystemBackend.dto.impl.VehicleDTO;
 import lk.ijse.CropMonitoringSystemBackend.entity.impl.VehicleEntity;
 import lk.ijse.CropMonitoringSystemBackend.exeption.DataPersistException;
+import lk.ijse.CropMonitoringSystemBackend.exeption.VehicleNotFoundException;
 import lk.ijse.CropMonitoringSystemBackend.service.VehicleService;
 import lk.ijse.CropMonitoringSystemBackend.util.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -33,5 +35,15 @@ public class VehicleServiceImpl implements VehicleService {
     public List<VehicleDTO> getAllVehicle() {
         List<VehicleEntity> allVehicle = vehicleDAO.findAll();
         return mapping.toVehicleDTOList(allVehicle);
+    }
+
+    @Override
+    public void deleteVehicle(String vehicleCode) {
+        Optional<VehicleEntity> vehicleEntity = vehicleDAO.findById(vehicleCode);
+        if (!vehicleEntity.isPresent()){
+            throw new VehicleNotFoundException("Vehicle not Found");
+        }else {
+            vehicleDAO.deleteById(vehicleCode);
+        }
     }
 }
