f437266aada70f19a62af2f3784f3e44924b2e6d
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-11-11 05:46:51 +0530

    create vehicle DAO

---
+++ src/main/java/lk/ijse/CropMonitoringSystemBackend/dao/VehicleDAO.java
@@ -0,0 +1,14 @@
+package lk.ijse.CropMonitoringSystemBackend.dao;
+
+import lk.ijse.CropMonitoringSystemBackend.entity.impl.VehicleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface VehicleDAO extends JpaRepository<VehicleEntity, String> {
+
+}
+
