337ea2100186c1a3aa426d2cbe596918597c562d
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-11-11 06:02:15 +0530

    create getAll vehicle endpoint

--- src/main/java/lk/ijse/CropMonitoringSystemBackend/service/VehicleService.java
+++ src/main/java/lk/ijse/CropMonitoringSystemBackend/service/VehicleService.java
@@ -2,6 +2,10 @@ package lk.ijse.CropMonitoringSystemBackend.service;
 
 import lk.ijse.CropMonitoringSystemBackend.dto.impl.VehicleDTO;
 
+import java.util.List;
+
 public interface VehicleService {
     void saveVehicle(VehicleDTO vehicleDTO);
+
+    List<VehicleDTO> getAllVehicle();
 }
